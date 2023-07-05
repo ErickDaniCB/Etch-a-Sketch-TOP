0b2fcd3eddc9fc08316bf2e46660f477994b1ad3
ErickDaniCB <erickdani10@hotmail.com>
2023-07-05 17:37:39 -0600

    Add interactive canvas on hover

--- eas.js
+++ eas.js
@@ -1,18 +1,26 @@
+const canvas = document.getElementById('canvas');
+const clear = document.getElementById('clear');
 
-// Function that fill the canvas with the given amount (16 by now) of square divs 
+// Function that fill the canvas with the given amount (only 16 by now) of square divs.
 function fillCanvas(number) {
 
+    
     for (let j = 1; j <= number; j++) {
         
         for (let i = 1; i <= number; i++) {
-            
+
             let div = document.createElement('div');
             div.classList.add('dot');
-            
-            const canvas = document.getElementById('canvas');
             canvas.appendChild(div);
+            div.addEventListener('mouseover', () => div.classList.add('colored') );
         }
     }
 }
 
 fillCanvas(16);
+
+let colored = document.querySelectorAll('.colored');
+function clearAll() {
+    colored.classList.remove('colored');
+}
+clear.addEventListener('click', clearAll);
