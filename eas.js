a291a4864f1a8a558c293a9f92995a1c78e372eb
ErickDaniCB <erickdani10@hotmail.com>
2023-07-14 17:25:13 -0600

    Add Clear All button

--- eas.js
+++ eas.js
@@ -1,28 +1,28 @@
 let pixelNum = 16;
 
 let container = document.querySelector('#container');
+let clearButton = document.querySelector('#clear');
 
-function canvasInteraction () {
- 
-    let canvas = document.createElement('div');
-    canvas.classList.add('canvas');
-    canvas.style.width = `${container.offsetWidth - 20}px`;
-    canvas.style.height = `${container.offsetWidth - 20}px`;
-    container.appendChild(canvas);
+let canvas = document.createElement('div');
+canvas.classList.add('canvas');
+canvas.style.width = `${container.offsetWidth - 20}px`;
+canvas.style.height = `${container.offsetWidth - 20}px`;
+container.appendChild(canvas);
 
-    let pixelDim = canvas.offsetWidth / pixelNum;
+let pixelDim = canvas.offsetWidth / pixelNum;
 
-    for (let i = 1; i <= pixelNum ** 2; i++) {
-        let pixel = document.createElement('div');
-        pixel.style.width = `${pixelDim}px`;
-        pixel.style.height = `${pixelDim}px`;
-        pixel.classList.add('pixel');
-        canvas.appendChild(pixel);
-        pixel.addEventListener('mouseover', () => pixel.style.backgroundColor = 'black');
-    }
-
-    
-
+for (let i = 1; i <= pixelNum ** 2; i++) {
+    let pixel = document.createElement('div');
+    pixel.style.width = `${pixelDim}px`;
+    pixel.style.height = `${pixelDim}px`;
+    pixel.classList.add('pixel');
+    canvas.appendChild(pixel);
+    pixel.addEventListener('mouseover', () => pixel.classList.add('colored'));
 }
 
-canvasInteraction();
+let pixelsNode = document.querySelectorAll('.pixel');
+function cleaner () {
+    pixelsNode.forEach((item) => item.classList.remove('colored'))
+}
+
+clearButton.addEventListener('click', cleaner);
