2a5aa74ab010424a10adc5f1e36e6a3632b9b786
ErickDaniCB <erickdani10@hotmail.com>
2023-07-18 16:57:11 -0600

    Add color picker and grid label

--- eas.js
+++ eas.js
@@ -1,11 +1,19 @@
 // Container
-let container = document.querySelector('#container');
-let canvas = document.createElement('div');
+const container = document.querySelector('#container');
+const canvas = document.createElement('div');
 canvas.classList.add('canvas');
 canvas.style.width = `${container.offsetWidth - 20}px`;
 canvas.style.height = `${container.offsetWidth - 20}px`;
 
-// Canvas
+//Canvas default color
+let color = document.querySelector('#color').value;
+
+//Canvas color changer
+function changeColor(newColor){
+    color = newColor;
+}
+
+// Canvas filling
 let pixelNum = 16;
 fillCanvas(pixelNum);
 
@@ -14,12 +22,12 @@ function fillCanvas(fill){
     container.appendChild(canvas);
     let pixelDim = canvas.offsetWidth / fill;
     for (let i = 1; i <= fill ** 2; i++) {
-        let pixel = document.createElement('div');
+        const pixel = document.createElement('div');
         pixel.style.width = `${pixelDim}px`;
         pixel.style.height = `${pixelDim}px`;
         pixel.classList.add('pixel');
         canvas.appendChild(pixel);
-        pixel.addEventListener('mouseover', () => pixel.classList.add('colored'));
+        pixel.addEventListener('mouseover', () => pixel.style.backgroundColor = `${color}`);
     }
 }
 
@@ -29,18 +37,29 @@ function emptyCanvas() {
     }
 }
 
-// Canvas density slider selector
+// Canvas density slider selector/label
+const sliderLabel = document.querySelector('#sliderLabel');
+sliderLabel.textContent = '16x16';
+
 function changePix (value) {
     pixelNum = value;
     emptyCanvas();
     fillCanvas(value);
+    sliderLabel.textContent = `${value}x${value}`;
 }
 
-// Clear All Button
-let clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', cleaner);
+// Restart Button
+const restartButton = document.querySelector('#restart');
+restartButton.addEventListener('click', restart);
 
-function cleaner () {
-    let pixelsNode = document.querySelectorAll('.pixel');
-    pixelsNode.forEach((item) => item.classList.remove('colored'))
+function restart () {
+    const pixelsNode = document.querySelectorAll('.pixel');
+    pixelsNode.forEach((item) => item.style.backgroundColor = 'White');
+}
+
+// Toggle rainbow button
+const rainbowToggle = document.querySelector('#rainbow');
+rainbowToggle.addEventListener('click', rainbow);
+function rainbow(){
+
 }
