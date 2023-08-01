0ebc2c9d85b5fe430bacd858a6b4ffa7018b5a6e
ErickDaniCB <erickdani10@hotmail.com>
2023-08-01 11:26:44 -0600

    Add toggles and labels for  grid and rainbow modes

--- eas.js
+++ eas.js
@@ -15,12 +15,12 @@ function changeColor(newColor){
 
 // Canvas filling
 let pixelNum = 16;
+let gridBtnTgl = document.querySelector('#gridBtn').value;
 fillCanvas(pixelNum);
 
 function fillCanvas(fill){
-    
     container.appendChild(canvas);
-    let pixelDim = canvas.offsetWidth / fill;
+    let pixelDim = canvas.offsetWidth / fill;   
     for (let i = 1; i <= fill ** 2; i++) {
         const pixel = document.createElement('div');
         pixel.style.width = `${pixelDim}px`;
@@ -28,6 +28,10 @@ function fillCanvas(fill){
         pixel.classList.add('pixel');
         canvas.appendChild(pixel);
         pixel.addEventListener('mouseover', () => pixel.style.backgroundColor = `${color}`);
+    }
+    if(gridBtnTgl == "ON"){
+        const pixelsNode = document.querySelectorAll('.pixel');
+        pixelsNode.forEach(item => item.classList.add('border'));
     }
 }
 
@@ -43,7 +47,7 @@ sliderLabel.textContent = '16x16';
 
 function changePix (value) {
     pixelNum = value;
-    emptyCanvas();
+    emptyCanvas();   
     fillCanvas(value);
     sliderLabel.textContent = `${value}x${value}`;
 }
@@ -54,12 +58,50 @@ restartButton.addEventListener('click', restart);
 
 function restart () {
     const pixelsNode = document.querySelectorAll('.pixel');
-    pixelsNode.forEach((item) => item.style.backgroundColor = 'White');
+    pixelsNode.forEach(item => item.style.backgroundColor = 'White');
 }
 
-// Toggle rainbow button
-const rainbowToggle = document.querySelector('#rainbow');
-rainbowToggle.addEventListener('click', rainbow);
-function rainbow(){
+// Toggle grid Button
+const GRID_LABEL = document.querySelector('#gridLabel');
+GRID_LABEL.textContent = `${gridBtnTgl}`;
 
+function toggleGrid () {
+    const pixelsNode = document.querySelectorAll('.pixel');
+    if(gridBtnTgl == "ON"){
+        pixelsNode.forEach(item => item.classList.remove('border'));
+        gridBtnTgl = "OFF";
+        GRID_LABEL.textContent = `${gridBtnTgl}`;
+    } 
+    else if(gridBtnTgl == "OFF"){
+        pixelsNode.forEach(item => item.classList.add('border'));
+        gridBtnTgl = "ON";
+        GRID_LABEL.textContent = `${gridBtnTgl}`;
+    }
+}
+// Toggle rainbow button
+let rbwBtnTgl = document.querySelector('#rainbow').value;
+const RAINBOW_LABEL = document.querySelector('#rainbowLabel');
+RAINBOW_LABEL.textContent = `${rbwBtnTgl}`;
+
+function colorRandom () {
+    let rdmColor = Math.floor(Math.random()*16777215).toString(16);
+    color = "#" + rdmColor;
+}
+
+function toggleRbw () {
+    const PIXELS_NODE = document.querySelectorAll('.pixel');
+    colorRandom();
+    if (rbwBtnTgl == "OFF") {
+        rbwBtnTgl = "ON";
+        PIXELS_NODE.forEach(item => {
+            item.addEventListener('mouseover', colorRandom);
+        });    
+    } else {
+        rbwBtnTgl = "OFF";
+        PIXELS_NODE.forEach(item => {
+            item.removeEventListener('mouseover', colorRandom);
+        });
+        color = document.querySelector('#color').value;
+    }  
+    RAINBOW_LABEL.textContent = `${rbwBtnTgl}`;
 }
